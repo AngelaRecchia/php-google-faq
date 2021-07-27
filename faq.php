439e27efda09f6e526f29b81b578d91b6e88ae5b
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-07-27 15:16:58 +0200

    init

---
+++ faq.php
@@ -0,0 +1,25 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Google FAQ</title>
+</head>
+<body>
+    <header>
+
+        <section id="top">
+            <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c51f.png" alt="google logo">
+            <span id="title">Privacy e termini</span>
+        </section>
+
+        <nav></nav>
+        
+
+    </header>
+</body>
+</html>
